c43b1393767a7df6b9d17b44ecf4beff15ca83f7
surafel_fikru <surafelfikru76@gmail.com>
2022-12-03 23:33:39 +0300

    fix endpoint names

--- ip_lookup/urls.py
+++ ip_lookup/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import all_view, unique_view
+from .views import full_ip_location_data_endpoint, unique_ip_location_data_endpoint
 
 urlpatterns = [
-    path("<str:ip>", all_view, name="all"),
-    path("<str:ip>/<str:key>", unique_view, name="unique"),
+    path("<str:ip>", full_ip_location_data_endpoint, name="all"),
+    path("<str:ip>/<str:key>", unique_ip_location_data_endpoint, name="unique"),
 ]
