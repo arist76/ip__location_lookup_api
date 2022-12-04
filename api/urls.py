f515d079423fc1dce3e20204ed659c0d0b59ca22
surafel_fikru <surafelfikru76@gmail.com>
2022-12-04 18:11:37 +0300

    Revert "fix endpoint names and ending slashes"
    This reverts commit f2052572bb9e85b581601524ec222b0c8955bff5.

--- api/urls.py
+++ api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("ip_lookup.urls")),
+    path("/", include("ip_lookup.urls")),
 ]
